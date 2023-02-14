76969c40025c9a5451b7937aee6c21f3ac5c31a7
Chris <74386161+TurnerCG@users.noreply.github.com>
2023-02-14 08:00:35 +0000

    Finished first Portfolio script.

---
+++ portfolio/week2/passwordCheck.sh
@@ -0,0 +1,28 @@
+#!/bin/bash 
+# Ask the user to type a secret password (Hide that input).
+# Check the user's password against the hash stored in 'secret.txt'.
+# If the user's password is correct, print "Access Granted" and quit with the exit code 0.
+# If the user's password is incorrect print "Access Denied" and quit with the exit code 1.
+
+#password is "password"
+
+echo -e "enter the password" 
+
+read -rsp "" password
+
+echo -n $password | sha256sum -c --status /workspaces/portfolio/portfolio/week2/TestOne/secret.txt
+
+
+
+if [ "$?" = "0" ] 
+    then
+        echo "Access Granted"
+    
+        exit 0
+    else
+        echo "Access Denied" 
+    
+        exit 1
+fi
+
+
